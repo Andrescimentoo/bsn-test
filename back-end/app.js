ffbeb3609918a51c7447f2f16d3c1574a67b57f1
Andre Nascimento <1914andre@gmail.com>
2025-06-17 20:46:33 -0300

    feat: adding middlewers and route to end points of the pokemons in app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,14 @@
-import express from "express" 
+import express, { json } from "express" 
+import cors from "cors";
+import { routeEndPointsPokemon } from "./src/routes/routesEndPointsPokenmon.routes"
 
 export const app = express()
 const port = 3000
+
+app.use(json())
+app.use(cors())
+
+app.use(routeEndPointsPokemon)
 
 app.listen(port, () => {
     console.log(`server it's running in ${port}`)
